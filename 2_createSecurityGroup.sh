088189df2f2b099f6ecdbbc18436d154d269bea7
burrsutter <burrsutter@gmail.com>
2018-07-09 14:32:30 -0700

    added centossetup.txt, removed some regions

--- 2_createSecurityGroup.sh
+++ 2_createSecurityGroup.sh
@@ -3,5 +3,5 @@
 
 export sgId=$(aws ec2 create-security-group --group-name FromCLI --description FromCLI --vpc-id ${vpcId} --output text)
 echo "SGID " $sgId
-aws ec2 authorize-security-group-ingress --group-id $sgId --protocol -1 --port -1 --cidr 0.0.0.0/0 --region=$awsregion
+aws ec2 authorize-security-group-ingress --group-id $sgId --protocol -1 --port -1 --cidr 0.0.0.0/0 
 aws ec2 create-tags --resources $sgId --tags Key=Name,Value=fromCLI
--- 4_runInstances.sh
+++ 4_runInstances.sh
@@ -2,7 +2,7 @@
 #amiId="ami-b81dbfc5"
 amiId="ami-4826c22b"
 instanceType="m5.xlarge"
-export instanceId=$(aws ec2 run-instances --no-dry-run \
+aws ec2 run-instances --no-dry-run \
                       --instance-type $instanceType \
                       --image-id $amiId \
                       --security-group-ids $sgId \
@@ -11,17 +11,12 @@ export instanceId=$(aws ec2 run-instances --no-dry-run \
                       --key-name devenv-key \
                       --block-device-mappings "DeviceName=/dev/sda1,Ebs={VolumeSize=250,VolumeType=gp2}" \
                       --associate-public-ip-address \
-                      --user-data file:///tmp/centossetup.txt \
+                      --user-data file:///./centossetup.txt \
                       --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=fromCLI}]' \
-                      --query 'Reservations[*].Instances[*][InstanceId]' --output text)
+                      --output json
 
-# unable to get the Instance id
-echo "InstanceID " $instanceId
-# aws ec2 create-tags --resources $instanceId --tags Key=Name,Value=fromCLI
-
-# 
-export publicIP=$(aws ec2 describe-instances --region=$awsregion --filters "Name=tag:Name,Values=fromCLI" --query "Reservations[*].Instances[*][PublicIpAddress]" --output text)
+export publicIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=fromCLI" --query "Reservations[*].Instances[*][PublicIpAddress]" --output text)
 echo "IP " $publicIP
-echo 'ssh -i devenv-key.pem centos@'$publicIP
+echo 'ssh -i devenv-key.pem centos@ip'
 echo "OR"
-echo 'ssh -i devenv-key.pem ec2-user@'$publicIP
+echo 'ssh -i devenv-key.pem ec2-user@ip'
